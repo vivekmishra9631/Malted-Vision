225a235e6994d6f760b07adbca6ada7fe69b7ea5
Prajapati Shrikrushna R. <Krishnaprajapati757@gmail.com>
2025-05-21 19:59:10 +0530

    footer changed

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Separator } from "@/components/ui/separator";
-// import { ChevronsDownIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
@@ -21,7 +20,7 @@ export const FooterSection = () => {
   return (
     <footer id="footer" className="container py-24 sm:py-32">
       <div className="max-w-2xl mx-auto p-6 bg-card border border-secondary rounded-2xl">
-        <div className="flex items-center justify-between">
+        <div className="flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-0">
           <Link href="#" className="flex font-bold items-center">
             <Image 
               src={resolvedTheme === "dark" ? "/mvwhite.png" : "/mvblack.png"}
@@ -31,18 +30,26 @@ export const FooterSection = () => {
               className="mr-2 rounded-lg border border-secondary"
               priority
             />
-            <h3 className="text-2xl">Malted Vision</h3>
+            <h3 className="text-xl sm:text-2xl">Malted Vision</h3>
           </Link>
           
-          <section>
-            <h3 className="text-sm">
+          <section className="text-center sm:text-right">
+            <h3 className="text-xs sm:text-sm md:text-base">
               Designed and Developed By{" "}
               <Link
                 target="_blank"
                 href="https://www.linkedin.com/in/vivek-mishra-27622a1b7/"
                 className="text-primary transition-all border-primary hover:border-b-2"
               >
-                Vivek Mishra
+                Vivek
+              </Link>{" "}
+              &{" "}
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/shrikrushna-p-89a667334/"
+                className="text-primary transition-all border-primary hover:border-b-2"
+              >
+                Shrikrushna
               </Link>
             </h3>
           </section>
