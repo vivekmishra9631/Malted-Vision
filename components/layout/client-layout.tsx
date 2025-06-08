204cd71cd791ff34c44659884e2279b73b1a4d28
Prajapati Shrikrushna R. <Krishnaprajapati757@gmail.com>
2025-06-08 10:11:10 +0530

    reduce loadig time

--- components/layout/client-layout.tsx
+++ components/layout/client-layout.tsx
@@ -11,7 +11,7 @@ export const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 3000);
+    }, 1200);
 
     return () => clearTimeout(timer);
   }, []);
